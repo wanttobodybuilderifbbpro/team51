e4158bfd1202abb9165a2df4472ca60275bbca24
jaeyooon <jycho0208@gmail.com>
2022-09-21 17:14:42 +0900

    modify test5.java

--- project/src/main/java/test/test5.java
+++ project/src/main/java/test/test5.java
@@ -3,5 +3,7 @@ package test;
 public class test5 {
 	public static void main(String[] args) {
 		System.out.println("test555");
+		System.out.println("test666");
+		System.out.println("test777");
 	}
 }
